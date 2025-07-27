f41a597415befff6dee32e6ba1b914bcaa3a17e9
Edo-analyst <matez4856@gmail.com>
2025-07-27 20:44:33 +0200

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -6,8 +6,8 @@ Script Purpose:
     This stored procedure performs the ETL (Extract, Transform, Load) process to 
     populate the 'silver' schema tables from the 'bronze' schema.
 	Actions Performed:
-		- Truncates Silver tables.
-		- Inserts transformed and cleansed data from Bronze into Silver tables.
+		1) Truncates Silver tables.
+		2) Inserts transformed and cleansed data from Bronze into Silver tables.
 		
 Parameters:
     None. 
