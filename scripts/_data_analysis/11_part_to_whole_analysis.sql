7ef6b62aa8cedd2aac1854a88a6c6171b15872f4
Edoardo <matez4856@gmail.com>
2025-09-19 11:40:57 +0200

    Update 11_part_to_whole_analysis.sql

--- scripts/_data_analysis/11_part_to_whole_analysis.sql
+++ scripts/_data_analysis/11_part_to_whole_analysis.sql
@@ -6,10 +6,6 @@ Purpose:
     - To compare performance or metrics across dimensions or time periods.
     - To evaluate differences between categories.
     - Useful for A/B testing or regional comparisons.
-
-SQL Functions Used:
-    - SUM(), AVG(): Aggregates values for comparison.
-    - Window Functions: SUM() OVER() for total calculations.
 ===============================================================================
 */
 -- Which categories contribute the most to overall sales?
@@ -28,4 +24,5 @@ SELECT
     SUM(total_sales) OVER () AS overall_sales,
     ROUND((CAST(total_sales AS FLOAT) / SUM(total_sales) OVER ()) * 100, 2) AS percentage_of_total
 FROM category_sales
+
 ORDER BY total_sales DESC;
